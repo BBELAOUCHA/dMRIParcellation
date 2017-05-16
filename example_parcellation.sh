0a8f779948805d33cfba0d5169761f473dc7e6a5
Brahim Belaoucha <brahim.belaoucha@gmail.com>
2017-05-16 10:34:19 +0200

    fix bug

--- example_parcellation.sh
+++ example_parcellation.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 data_folder=${PWD}
-MNNparcellation  -i "$data_folder/data/W_cgal.mat" -o "$data_folder/data/Results/" -t "$data_folder/data/tract" -tb "tract_" -seed "$data_folder/data/tract/fdt_coordinates.txt" -n 10 -Ex "$data_folder/data/Excluded_points.txt" -nodif "$data_folder/data/nodif_brain_mask.nii.gz" -sm 'Tanimoto' -v 1 -m 0
+MNNparcellation  -i "$data_folder/test/data/W_cgal.mat" -o "$data_folder/test/data/Results/" -t "$data_folder/test/data/tract" -tb "tract_" -seed "$data_folder/test/data/tract/fdt_coordinates.txt" -n 10 -Ex "$data_folder/test/data/Excluded_points.txt" -nodif "$data_folder/test/data/nodif_brain_mask.nii.gz" -sm 'Tanimoto' -v 1 -m 0
 
